4132fe5f0b6b18c5e42de7f6c60f5a364896eb4f
Chris <33527729+chaboii@users.noreply.github.com>
2020-05-09 00:35:09 -0400

    idk

--- gunGame/classes/Item.cs
+++ gunGame/classes/Item.cs
@@ -1,9 +1,12 @@
+using System.Runtime.CompilerServices;
+
 namespace gunGame
 {
     class Item
     {
         //Items in general
         //Also defining stats of all items
+        int _ID;
         string _name;
         int _dmg;
 
